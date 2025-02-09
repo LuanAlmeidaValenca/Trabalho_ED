e6d17030a8cc3188442ebbe2382857c7466994ce
LuanAlmeidaValenca <luan.valenca@hotmail.com>
2025-02-09 11:32:48 -0300

    condigo corrigido

--- TrabalhoBanco.c
+++ TrabalhoBanco.c
@@ -1,56 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct no{
     int horario[4], PFPJ, preferencial;
     char nome[40];
-    Cliente *prox;
-} Cliente;
+    struct no *prox;
+} No;
 
 typedef struct fila_banco{
     int tamanho;
-    Cliente *primeiro;
+    No *primeiro;
 } Fila;
 
-Fila criarFila(Fila *fila){
-    fila = (Fila *) malloc(sizeof(Fila));
-    if(fila){
+void criarFila(Fila *fila){
         fila->tamanho = 0;  
         fila->primeiro = NULL;  
-    }
-    return *fila;
+        printf("\nFila criada com sucesso\n");
 }
 
-void imprimirFila(Fila *fila) {
-    if (fila->tamanho == 0) {
-        printf("\nFila vazia\n");
-        return;
-    }
+void imprimirFila(Fila fila) {
+    No *no = fila.primeiro;   
 
-    Cliente *no = fila->primeiro;
-    printf("\n--- FILA DE CLIENTES ---\n");
+    if (!no){
+        printf("\nFila vazia\n");
+    }
     
-    while (no != NULL) {
-        printf("-------------------\n");
+    printf("\n----- FILA DE CLIENTES -----\n");
+    
+    while (no) {
+        printf("----------------------------\n");
         printf("Nome: %s\n", no->nome);
-        printf("Preferência: %d\n", no->preferencial);
-        printf("É PF ou PJ: %d\n", no->PFPJ);
-        printf("Horário de chegada: %d%d:%d%d\n", 
+        if(no->preferencial){
+            printf("Eh Preferencial\n");
+        }else{
+            printf("Nao eh preferencial\n");
+        }
+        if(no->PFPJ){
+            printf("Pessoa fisica\n");
+        }else{
+            printf("Pessoa juridica\n'");
+        }
+        printf("Horario de chegada: %d%d:%d%d\n", 
                no->horario[0], no->horario[1], no->horario[2], no->horario[3]);
-        printf("-------------------\n");
         
         no = no->prox;
     }
+    printf("----------------------------\n");
 }
 
-void inserirNaFila(Fila *fila, char nome, int hora, int PFPJ, int preferencial){
-    Cliente *novo = (Cliente *)malloc(sizeof(Cliente));
+
+void inserirNaFila(Fila *fila, char *nome, int hora[4], int PFPJ, int preferencial){
+    No *aux, *novo = (No *)malloc(sizeof(No));
     if (!novo) {
-        printf("Erro ao alocar memória!\n");
+        printf("\nErro ao alocar memoria!\n");
         return;
     }
 
     strcpy(novo->nome, nome);
-    memcpy(novo->horario, &hora, 4 * sizeof(int));
+    memcpy(novo->horario, hora, 4 * sizeof(int));
     novo->PFPJ = PFPJ;
     novo->preferencial = preferencial;
     novo->prox = NULL;
@@ -58,35 +66,48 @@ void inserirNaFila(Fila *fila, char nome, int hora, int PFPJ, int preferencial){
     if (fila->primeiro == NULL) {
         fila->primeiro = novo;
     } else {
-        Cliente *temp = fila->primeiro;
-        while (temp->prox != NULL) {
-            temp = temp->prox;
+        aux = fila->primeiro;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
         }
-        temp->prox = novo;
+        aux->prox = novo;
     }
 
     fila->tamanho++;
+    printf("\nCliente adicionado com sucesso\n");
 }
 
-void removerDaFila();
+void removerDaFila(Fila *fila){
+    if (fila->primeiro == NULL) {
+        printf("Lista vazia!\n");
+        return;
+    }
+
+    No *temp = fila->primeiro;
+    fila->primeiro = fila->primeiro->prox;
+    free(temp);
+    fila->tamanho--;
+}
+
 void verPrevisao();
 void atender();
 void juntarFilas();
 void separarFilas();
 
 void liberarMemoria(Fila *fila) {
-    Cliente *temp = fila->primeiro;
+    No *temp = fila->primeiro;
     while (temp != NULL) {
-        Cliente *remover = temp;
+        No *remover = temp;
         temp = temp->prox;
         free(remover);
     }
     fila->tamanho = 0;
     fila->primeiro = NULL;
+    printf("\nMemoria liberada!\n");
 }
 
 int main() {
-    int escolha, hora[4], PFouPJ, preferencial;
+    int escolha = 0, hora[4], PFouPJ, preferencial;
     Fila fila;
     char nome[30];
     
@@ -101,12 +122,9 @@ int main() {
         printf("\n7 - Unir filas");
         printf("\n8 - Separar filas");
         printf("\n9 - Encerrar atendimento");
-        printf("\nEscolha a opção desejada: ");
+        printf("\n\nEscolha a opção desejada: ");
         scanf("%d", &escolha);
-        
-        if(escolha < 1 && escolha > 9){
-            printf("Escolha inválida, o número precisa estar entre 1 e 9");
-        }
+        getchar();
         
         switch (escolha){
             case 1: 
@@ -114,38 +132,57 @@ int main() {
             break;
             
             case 2: 
-                imprimirFila(&fila);
+                imprimirFila(fila);
             break;
             
             case 3: 
+                printf("\nInsira o nome do cliente: ");
+                fgets(nome, sizeof(nome), stdin);
+                printf("\nInsira o horario de chegada do cliente (Sem ':'): ");
+                scanf("%1d%1d%1d%1d", &hora[0], &hora[1], &hora[2], &hora[3]);
+                getchar();
+                printf("\nInsira se o cliente eh PF ou PJ (1 para PF, 0 para PJ): ");
+                scanf("%d", &PFouPJ);
+                getchar();
+                if(PFouPJ != 1 && PFouPJ != 0){
+                    printf("\nOpcao invalida\n");
+                    break;
+                }
+                printf("\nInsira se o cliente eh preferencial (1 para sim, 0 para nao): ");
+                scanf("%d", &preferencial);
+                getchar();
+                if(preferencial != 1 && preferencial != 0){
+                    printf("\nOpcao invalida\n");
+                    break;
+                }
                 inserirNaFila(&fila, nome, hora, PFouPJ, preferencial);
             break;
             
             case 4: 
-                removerDaFila();
+                removerDaFila(&fila);
             break;
             
             case 5: 
-                verPrevisao();
+                //verPrevisao();
             break;
             
             case 6: 
-                atender();
+                //atender();
             break;
             
             case 7:
-                juntarFilas();
+                //juntarFilas();
             break;
             
             case 8: 
-                separarFilas();
+                //separarFilas();
             break;
             
             case 9:
                 liberarMemoria(&fila);
             break;
             default:
-            printf("Opção inválida");
+            printf("\nOpção inválida\n");
         }
     }
 
